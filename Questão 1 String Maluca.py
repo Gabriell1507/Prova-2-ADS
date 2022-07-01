996d69e13b8917ae8706fad8d7bec97b57cbd042
Gabriell1507 <105975414+Gabriell1507@users.noreply.github.com>
2022-07-01 16:34:53 -0300

    Add files via upload

--- Questão 1 String Maluca.py
+++ Questão 1 String Maluca.py
@@ -1,12 +1,15 @@
 def crazy_string():
     from random import choice
     entrada = input('Insira a string: ')
-    
+    entrada2 = input('Insira a outra string: ')
     saída = ''.join(choice((str.upper, str.lower))  #Utilizando o join para juntar a string, e com ajuda do random choice, junto ao laço for abaixo, para alternar entre maiúsculo e minúsculo.
+    (letra) for letra in entrada)
+    saída2 = ''.join(choice((str.upper, str.lower))  #Imprimindo a segunda saída
     (letra) for letra in entrada)
     print('Imprimindo a saída...') #Imprimindo o resultado de como fica a string.
     print('\033[36m')
     print(saída)
+    print(saída2)
     
 crazy_string()
 
